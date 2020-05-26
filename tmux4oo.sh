706b0e9d073218d0727b71b9b1be0978dfca0ce9
GK102 <33454655+GK102@users.noreply.github.com>
2020-05-26 13:06:10 +0700

    Update tmux4oo.sh

--- tmux4oo.sh
+++ tmux4oo.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-tmux new-session -d -s htop-session 'htop';  # start new detached tmux session, run htop
-tmux split-window;                             # split the detached tmux session
-tmux send 'htop -t' ENTER;                     # send 2nd command 'htop -t' to 2nd pane. I believe there's a `--target` option to target specific pane.
-tmux a;                                        # open (attach) tmux session.
+# var for session name (to avoid repeated occurences)
+sn=xyz
+
+# Start the session and window 0 in /etc
+#   This will also be the default cwd for new windows created
+#   via a binding unless overridden with default-path.
+cd /etc
+tmux new-session -s "$sn" -n etc -d
+
+# Create a bunch of windows in /var/log
+cd /var/log
+for i in {1..6}; do
+    tmux new-window -t "$sn:$i" -n "var$i"
+done
+
+# Set the default cwd for new windows (optional, otherwise defaults to session cwd)
+#tmux set-option default-path /
+
+# Select window #1 and attach to the session
+tmux select-window -t "$sn:1"
+tmux -2 attach-session -t "$sn"
