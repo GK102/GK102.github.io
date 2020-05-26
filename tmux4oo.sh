449e6ee6ceb7860aafb31536e961958a05251b6a
GK102 <33454655+GK102@users.noreply.github.com>
2020-05-26 12:53:21 +0700

    Update tmux4oo.sh

--- tmux4oo.sh
+++ tmux4oo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 tmux new-session -d  
-tmux send-keys 'bundle exec thin start' 'C-m'
-tmux rename-window 'Foo'
-tmux select-window -t foo:0
-tmux split-window -h 'exec pfoo'
-tmux send-keys 'bundle exec compass watch' 'C-m'
-tmux split-window -v -t 0 'exec pfoo'
-tmux send-keys 'rake ts:start' 'C-m'
-tmux split-window -v -t 1 'exec pfoo'
-tmux -2 attach-session -t foo
+tmux send-keys 
+tmux rename-window 
+tmux select-window -t 
+tmux split-window -h
+tmux send-keys 
+tmux split-window -v 
+tmux send-keys 
+tmux split-window -v 
+
