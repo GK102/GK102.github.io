b1e2dddb4913499eb24fa95b629d1ac64167e26f
GK102 <33454655+GK102@users.noreply.github.com>
2020-05-26 13:51:48 +0700

    Update tmux4oo.sh

--- tmux4oo.sh
+++ tmux4oo.sh
@@ -2,9 +2,9 @@
 tmux new-window -a -n WinSplit
 tmux new-session -d -s WinSplit
 tmux selectp -t WinSplit
-tmux split-window -v
+tmux split-window -h
 tmux set-window-option -g window-status-current-bg blue
 tmux split-window -v
-tmux split-window -v
+tmux split-window -h
 tmux select-layout even-vertical
 tmux attach -t WinSplit
