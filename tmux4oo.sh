2dcb14ef87045d95df425d622375bfb39363e62d
GK102 <33454655+GK102@users.noreply.github.com>
2020-05-26 13:13:43 +0700

    Update tmux4oo.sh

--- tmux4oo.sh
+++ tmux4oo.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-# var for session name (to avoid repeated occurences)
-sn=xyz
+#
+# Setup a work space called `work` with two windows
+# first window has 3 panes. 
+# The first pane set at 65%, split horizontally, set to api root and running vim
+# pane 2 is split at 25% and running redis-server 
+# pane 3 is set to api root and bash prompt.
+# note: `api` aliased to `cd ~/path/to/work`
+#
+session="work"
 
-# Start the session and window 0 in /etc
-#   This will also be the default cwd for new windows created
-#   via a binding unless overridden with default-path.
-cd /etc
-tmux new-session -s "$sn" -n etc -d
+# set up tmux
+tmux start-server
 
-# Create a bunch of windows in /var/log
-cd /var/log
-for i in {1..6}; do
-    tmux new-window -t "$sn:$i" -n "var$i"
-done
+# create a new tmux session, starting vim from a saved session in the new window
+tmux new-session -d -s $session -n vim #"vim -S ~/.vim/sessions/kittybusiness"
 
-# Set the default cwd for new windows (optional, otherwise defaults to session cwd)
-#tmux set-option default-path /
+# Select pane 1, set dir to api, run vim
+tmux selectp -t 1 
+tmux send-keys "api;vim" C-m 
 
-# Select window #1 and attach to the session
-tmux select-window -t "$sn:1"
-tmux -2 attach-session -t "$sn"
+# Split pane 1 horizontal by 65%, start redis-server
+tmux splitw -h -p 35
+tmux send-keys "redis-server" C-m 
+
+# Select pane 2 
+tmux selectp -t 2
+# Split pane 2 vertiacally by 25%
+tmux splitw -v -p 75
+
+# select pane 3, set to api root
+tmux selectp -t 3
+tmux send-keys "api" C-m 
+
+# Select pane 1
+tmux selectp -t 1
+
+# create a new window called scratch
+tmux new-window -t $session:1 -n scratch
+
+# return to main vim window
+tmux select-window -t $session:0
+
+# Finished setup, attach to the tmux session!
+tmux attach-session -t $session
